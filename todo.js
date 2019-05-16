16f62fa58be8125c5310a9793334b7e7b0016771
brian ruddy <briancruddy@gmail.com>
2019-05-16 11:09:26 -0400

    init

---
+++ todo.js
@@ -0,0 +1,50 @@
+// helper function to get a single element
+function $ (selector) {
+    return document.querySelector(selector);
+}
+
+// helper function to get all elements matching the selector
+function $$ (selector) {
+    return Array.from(document.querySelectorAll(selector));
+}
+
+
+function listenHomepage () {
+    $('.todo-form--add').addEventListener('click', function () {
+        const inputField = $('.todo-form--input');
+        const text = inputField.value;
+        console.log(text);
+
+        addItem(text);
+    });
+
+    console.log('homepage listeners set');
+}
+
+function addItem (text) {
+    const newItem = document.createElement('li');
+    newItem.textContent = text;
+
+    const buttons = createItemButtons();
+    buttons.forEach(button => {
+        newItem.appendChild(button)
+    });
+
+    $('.todo-list').appendChild(newItem);
+}
+
+function createItemButtons () {
+    const done = document.createElement('button');
+    done.classList.add('todo-list--item--done');
+    done.textContent = 'done';
+
+    const remove = document.createElement('button');
+    remove.classList.add('todo-list--item--remove');
+    remove.textContent = 'remove';
+
+    return [done, remove];
+}
+
+function markItemDone (event) {}
+
+function removeItem (event) {}
